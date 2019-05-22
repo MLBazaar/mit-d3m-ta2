945a4852d0b84d721d18873efc595ec3ae760722
Hector Dominguez <hedb_12@hotmail.com>
2019-05-22 14:25:32 -0400

    test for `CoreServicer._run_session`

--- tests/unit/ta3/test_core_servicer.py
+++ tests/unit/ta3/test_core_servicer.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from unittest.mock import MagicMock, patch
 
 from google.protobuf.timestamp_pb2 import Timestamp
 from ta3ta2_api import core_pb2_grpc
@@ -32,3 +33,51 @@ def test_core_servicer():
     assert instance.output_dir == output_dir
     assert instance.timeout == timeout
     assert not instance.debug
+
+
+@patch('ta2.ta3.core_servicer.LOGGER.exception')
+@patch('ta2.ta3.core_servicer.LOGGER.info')
+def test_core_servicer_run_session(logger_info_mock, logger_exception_mock):
+    session = {'type': 'test-type', 'id': 'test-id'}
+
+    # no arguments
+    method = MagicMock()
+
+    instance = CoreServicer('/input-dir', '/output-dir', 0.5)
+    instance._run_session(session, method)
+
+    args, kwargs = method.call_args
+
+    assert method.call_count == 1
+    assert logger_info_mock.call_count == 1
+    assert logger_exception_mock.call_count == 0
+    assert args == ()
+    assert kwargs == {}
+    assert 'end' in session
+    assert 'done' in session
+    assert 'error' not in session
+
+    # with arguments
+    instance._run_session(session, method, 'first-argument', second='argument')
+
+    args, kwargs = method.call_args
+
+    assert method.call_count == 2
+    assert logger_info_mock.call_count == 2
+    assert logger_exception_mock.call_count == 0
+    assert args == ('first-argument',)
+    assert kwargs == {'second': 'argument'}
+    assert 'end' in session
+    assert 'done' in session
+    assert 'error' not in session
+
+    # with exception
+    method = MagicMock(side_effect=IndexError)
+    instance._run_session(session, method)
+
+    assert method.call_count == 1
+    assert logger_info_mock.call_count == 3
+    assert logger_exception_mock.call_count == 1
+    assert 'end' in session
+    assert 'done' in session
+    assert 'error' in session
