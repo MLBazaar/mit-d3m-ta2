8b4448231673c25c0988850f63ec83cd8018bcea
Carles Sala <c.sala.stq@gmail.com>
2020-02-13 07:27:54 +0100

    new generate templates scripts

--- scripts/generate_templates.py
+++ scripts/generate_templates.py
@@ -1,10 +1,12 @@
+import argparse
 import os
-import traceback
 import sys
-
-import pandas as pd
+import traceback
+from datetime import datetime, timezone
 
 from d3m.metadata.pipeline import Pipeline
+
+TUNING_PARAMETER = 'https://metadata.datadrivendiscovery.org/types/TuningParameter'
 
 
 def load_pipeline(pipeline):
@@ -14,40 +16,73 @@ def load_pipeline(pipeline):
         else:
             pipeline = Pipeline.from_yaml(_pipeline)
 
-    pipeline.id = pipeline.get_digest()
+    return pipeline
+
+
+def get_default_step_hyperparams(step):
+    default_tunable_hyperparams = {}
+    for name, hp in step.get_all_hyperparams().items():
+        if TUNING_PARAMETER not in hp.semantic_types:
+            continue
+
+        default_tunable_hyperparams[name] = hp.get_default()
+
+    return default_tunable_hyperparams
+
+
+def clean_hyperparams(pipeline):
+    for step in pipeline.steps:
+        default_tunable_hyperparams = get_default_step_hyperparams(step)
+
+        for name, value in step.hyperparams.items():
+            if name in default_tunable_hyperparams.keys():
+                value['data'] = default_tunable_hyperparams[name]
 
     return pipeline
 
 
-def generate_new_templates(path):
-    errors = []
-    for pipeline in os.listdir(path):
-        pipeline_path = os.path.join(path, pipeline)
+def pipeline_to_template(pipeline_path):
+    pipeline = load_pipeline(pipeline_path)
+    template = clean_hyperparams(pipeline)
+
+    template.id = ''
+    template.schema = 'https://metadata.datadrivendiscovery.org/schemas/v0/pipeline.json'
+    template.created = datetime(2016, 11, 11, 12, 30, tzinfo=timezone.utc)
+
+    return template
+
+
+def write_template(templates_path, template):
+    template_id = template.get_digest()[:12]
+    template_path = os.path.join(templates_path, template_id + '.json')
+
+    with open(template_path, 'w') as template_file:
+        print("Creating template {}".format(template_path))
+        template.to_json(template_file)
+
+
+def generate_templates(pipelines_path, templates_path):
+    for pipeline in os.listdir(pipelines_path):
+        pipeline_path = os.path.join(pipelines_path, pipeline)
         try:
-            pipeline = load_pipeline(pipeline_path)
-            new_path = os.path.join('templates', pipeline.id + '.json')
+            template = pipeline_to_template(pipeline_path)
+            write_template(templates_path, template)
+        except Exception as ex:
+            print(ex)
 
-            with open(new_path, 'w') as pipeline_file:
-                pipeline.to_json(pipeline_file)
 
-            print('OK:', pipeline.id)
+def parse_args():
+    parser = argparse.ArgumentParser(description='Generate templates from pipelines')
+    parser.add_argument('pipelines_path', help='Path to the pipelines folder')
+    parser.add_argument('templates_path', help='Path to the templates folder')
 
-        except Exception:
-            print('ERROR:', pipeline)
-            traceback.print_exc(file=sys.stdout)
-            errors.append(pipeline_path)
+    return parser.parse_args()
 
-    if errors:
-        with open('errors.txt', 'w') as f:
-            for error in errors:
-                print(error, file=f)
+
+def main():
+    args = parse_args()
+    generate_templates(args.pipelines_path, args.templates_path)
 
 
 if __name__ == '__main__':
-
-    # Make all the d3m traces quiet
-    null = open(os.devnull, 'w')
-    sys.stderr = null
-
-    path = sys.argv[1]
-    generate_new_templates(path)
+    main()
