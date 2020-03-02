baad7abdf4888a8c59d9aebb4cf7de95a164b671
JDTheRipperPC <jdtheripperpc@gmail.com>
2020-03-02 15:59:56 +0100

    update test_core WIP

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,11 +2,13 @@ from datetime import datetime, timedelta
 from unittest import TestCase
 from unittest.mock import MagicMock, Mock, call, patch
 
-import pytest
 import numpy as np
+import pytest
+from btb.tuning import StopTuning
 
-from ta2.core import (DATAMART_URL, DATA_PIPELINE_PATH, DEFAULT_SCORING_PIPELINE_PATH,
-                      TEMPLATES_DIR, ScoringError, TA2Core)
+from ta2.core import (
+    DATA_PIPELINE_PATH, DATAMART_URL, DEFAULT_SCORING_PIPELINE_PATH, TEMPLATES_CSV, TEMPLATES_DIR,
+    ScoringError, TA2Core)
 
 
 class TestTA2Core(TestCase):
@@ -46,6 +48,7 @@ class TestTA2Core(TestCase):
         mock_open.assert_called_once_with('abs_path/demo-template.json', 'r')
         mock_json.load.assert_called_once_with(mock_open.return_value.__enter__())
 
+    @pytest.mark.skip(reason="TODO")
     def test__select_templates(self):
         pass  # TODO
 
@@ -188,28 +191,42 @@ class TestTA2Core(TestCase):
         mock_d3m_evaluate.assert_called_once_with(pipeline)
         out.extend.assert_called_once_with(mock_d3m_evaluate.return_value)
 
+    @pytest.mark.skip(reason="TODO")
     def test_subprocess_evaluate(self):
         pass  # TODO
 
     def test_score_pipeline_failed(self):
         # setup
+        failed_result = MagicMock()
+        failed_result.pipeline_run.status = {'message': 'Test Error Message'}
+
         instance = MagicMock(autospec=TA2Core)
         instance.folds = 5
-        instance.subprocess_evaluate.return_value = (False, [MagicMock()])
+
+        # we add 1 on front to make sure that we are getting the last element.
+        instance.subprocess_evaluate.return_value = (False, [1, failed_result])
 
         # run
         dataset = dict()
         problem = {'problem': {'performance_metrics': None}}
         pipeline = MagicMock()
 
-        with pytest.raises(ScoringError):
+        with pytest.raises(ScoringError) as exception_info:
             TA2Core.score_pipeline(instance, dataset, problem, pipeline)
 
-    @patch('yaml.dump_all')
+        assert str(exception_info.value) == 'Test Error Message'
+
+    @patch('ta2.core.yaml')
     @patch('ta2.core.open')
     def test_score_pipeline_dump_summary(self, mock_file, mock_yaml):
         # setup
-        all_results = [Mock(), Mock()]
+        result_0 = MagicMock()
+        result_0.pipeline_run.to_json_structure.return_value = {'result': 0}
+
+        result_1 = MagicMock()
+        result_1.pipeline_run.to_json_structure.return_value = {'result': 1}
+
+        all_results = [result_0, result_1]
 
         score_1 = MagicMock()
         score_1.value = [1, 1, 1]
@@ -230,24 +247,20 @@ class TestTA2Core(TestCase):
         metric = Mock()
         problem = {'problem': {'performance_metrics': [{'metric': metric}]}}
         pipeline = MagicMock()
+        pipeline.id = 1
 
-        TA2Core.score_pipeline(
-            instance, dataset, problem, pipeline
-        )
+        TA2Core.score_pipeline(instance, dataset, problem, pipeline)
 
         # assert
-        expected_runs = [
-            all_results[0].pipeline_run.to_json_structure(),
-            all_results[1].pipeline_run.to_json_structure()
-        ]
+        expected_runs = [{'result': 0}, {'result': 1}]
 
         assert pipeline.cv_scores == [1, 2]
         assert pipeline.score == 1.5
 
-        mock_file.assert_called_once_with('output/pipeline_runs/{}.yml'.format(pipeline.id), 'w')
-        mock_yaml.assert_called_once_with(
+        mock_file.assert_called_once_with('output/pipeline_runs/1.yml', 'w')
+        mock_yaml.dump_all.assert_called_once_with(
             expected_runs,
-            mock_file().__enter__(),
+            mock_file.return_value.__enter__(),
             default_flow_style=False
         )
 
@@ -294,7 +307,7 @@ class TestTA2Core(TestCase):
 
         # assert
         expected_pipeline_dict = {
-            'rank': (1 - 0.5) + 0.1 * 1.e-12,
+            'rank': 0.5000000000001,
             'score': 0.5,
             'normalized_score': 0.5
         }
@@ -321,6 +334,7 @@ class TestTA2Core(TestCase):
 
         instance = MagicMock(autospec=TA2Core)
         instance.dump = True
+        instance.solutions = list()
 
         pipeline = Mock()
         pipeline.score = 0.5
@@ -334,22 +348,23 @@ class TestTA2Core(TestCase):
 
         # assert
         expected_pipeline_dict = {
-            'rank': (1 - 0.5) + 0.1 * 1.e-12,
+            'rank': 0.5000000000001,
             'score': 0.5,
             'normalized_score': 0.5
         }
 
         expected_dump_pipeline_dicts = [dict(), dict()]
         expected_dump_pipeline_dirs = [instance.scored_dir, instance.ranked_dir]
-        expected_dump_pipeline_ranks = [(1 - 0.5) + 0.1 * 1.e-12]
+        expected_dump_pipeline_ranks = [0.5000000000001]
 
         assert self.called_with_pp_dict == expected_dump_pipeline_dicts
         assert self.called_with_dirs == expected_dump_pipeline_dirs
         assert self.ranks == expected_dump_pipeline_ranks
 
         pipeline.to_json_structure.assert_called_once_with()
-        instance.solutions.append.assert_called_once_with(expected_pipeline_dict)
+        assert instance.solutions == [expected_pipeline_dict]
 
+    @pytest.mark.skip(reason="TODO")
     def test__new_pipeline(self):
         pass  # TODO
 
@@ -362,7 +377,7 @@ class TestTA2Core(TestCase):
         # run
         TA2Core._check_stop(instance)
 
-    def test__check_stop_true(self):
+    def test__check_stop_to_stop(self):
         # setup
         instance = MagicMock(autospec=TA2Core)
         instance._stop = True
@@ -397,14 +412,16 @@ class TestTA2Core(TestCase):
     def test_stop_no_subprocess(self):
         # setup
         instance = MagicMock(autospec=TA2Core)
-        instance.subprocess = None
+        instance.subprocess = False
 
         # run
         TA2Core.stop(instance)
 
         # assert
         assert instance._stop
-        assert instance.subprocess is None
+        assert not instance.subprocess
+        # To make sure that False has not been changed to None.
+        assert type(instance.subprocess) == bool
 
     def test__timeout(self):
         # setup
@@ -414,7 +431,8 @@ class TestTA2Core(TestCase):
         # run
         with pytest.raises(KeyboardInterrupt):
             TA2Core._timeout(instance)
-            assert instance.killed
+
+        assert instance.killed
 
     @patch('ta2.core.datetime')
     def test__setup_search_without_timeout(self, mock_datetime):
@@ -463,7 +481,7 @@ class TestTA2Core(TestCase):
         # assert
         expected_max_end_time = datetime_now + timedelta(seconds=5)
 
-        assert instance.timeout is 5
+        assert instance.timeout == 5
         assert instance.solutions == list()
         assert instance.summary == list()
         assert not instance._stop
@@ -501,7 +519,7 @@ class TestTA2Core(TestCase):
         # assert
         expected_max_end_time = datetime_now + timedelta(seconds=5)
 
-        assert instance.timeout is 5
+        assert instance.timeout == 5
         assert instance.solutions == list()
         assert instance.summary == list()
         assert not instance._stop
@@ -623,5 +641,322 @@ class TestTA2Core(TestCase):
         datamart.search_with_data.assert_called_once_with(query=query, supplied_data=dict())
         cursor.get_next_page.assert_called_once_with()
 
+    @pytest.mark.skip(reason="TODO")
     def test__make_btb_scorer(self):
         pass  # TODO
+
+    @patch('ta2.core.BTBSession')
+    @patch('ta2.core.LazyLoader')
+    def test__start_session_no_budget(self, mock_lazy_loader, mock_btb_session):
+        # setup
+        instance = MagicMock(autospec=TA2Core)
+        instance.max_errors = 5
+
+        btb_scorer = Mock()
+        instance._make_btb_scorer.return_value = btb_scorer
+
+        template_loader = Mock()
+        mock_lazy_loader.return_value = template_loader
+
+        session = Mock()
+        mock_btb_session.return_value = session
+
+        # run
+        template_names = ['template_1', 'template_2']
+        dataset = 'demo-dataset'
+        problem = {'problem': dict()}
+        budget = None
+
+        TA2Core._start_session(instance, template_names, dataset, problem, budget)
+
+        # test
+        mock_lazy_loader.assert_called_once_with(['template_1', 'template_2'], TEMPLATES_DIR)
+        instance._make_btb_scorer.assert_called_once_with(
+            'demo-dataset', {'problem': dict()}, mock_lazy_loader.return_value)
+        mock_btb_session.assert_called_once_with(template_loader, btb_scorer, max_errors=5)
+        session.run.assert_called_once_with()
+
+    @patch('ta2.core.BTBSession')
+    @patch('ta2.core.LazyLoader')
+    def test__start_session_with_budget_no_ignore_error(self, mock_lazy_loader, mock_btb_session):
+        # setup
+        instance = MagicMock(autospec=TA2Core)
+        instance.max_errors = 5
+        instance.spent = 0
+        instance.ignore_errors = False
+
+        btb_scorer = Mock()
+        instance._make_btb_scorer.return_value = btb_scorer
+
+        template_loader = Mock()
+        mock_lazy_loader.return_value = template_loader
+
+        session = Mock()
+        session.proposals.values.side_effect = [[{'score': 1}], [{'score': None}], [{'score': 3}]]
+        mock_btb_session.return_value = session
+
+        # run
+        template_names = ['template_1', 'template_2']
+        dataset = 'demo-dataset'
+        problem = {'problem': dict()}
+        budget = 3
+
+        TA2Core._start_session(instance, template_names, dataset, problem, budget)
+
+        # assert
+        expected_run_call_args_list = [call(1), call(1), call(1)]
+
+        mock_lazy_loader.assert_called_once_with(['template_1', 'template_2'], TEMPLATES_DIR)
+        instance._make_btb_scorer.assert_called_once_with(
+            'demo-dataset', {'problem': dict()}, mock_lazy_loader.return_value)
+        mock_btb_session.assert_called_once_with(template_loader, btb_scorer, max_errors=5)
+
+        assert session.run.call_args_list == expected_run_call_args_list
+
+    @patch('ta2.core.BTBSession')
+    @patch('ta2.core.LazyLoader')
+    def test__start_session_with_budget_ignore_error(self, mock_lazy_loader, mock_btb_session):
+        # setup
+        instance = MagicMock(autospec=TA2Core)
+        instance.max_errors = 5
+        instance.spent = 0
+        instance.ignore_error = True
+
+        btb_scorer = Mock()
+        instance._make_btb_scorer.return_value = btb_scorer
+
+        template_loader = Mock()
+        mock_lazy_loader.return_value = template_loader
+
+        session = Mock()
+        session.proposals.values.side_effect = [
+            [{'score': 1}],
+            [{'score': None}],
+            [{'score': 3}],
+            [{'score': 4}]
+        ]
+        mock_btb_session.return_value = session
+
+        # run
+        template_names = ['template_1', 'template_2']
+        dataset = 'demo-dataset'
+        problem = {'problem': dict()}
+        budget = 3
+
+        TA2Core._start_session(instance, template_names, dataset, problem, budget)
+
+        # assert
+        expected_run_call_args_list = [call(1), call(1), call(1), call(1)]
+
+        mock_lazy_loader.assert_called_once_with(['template_1', 'template_2'], TEMPLATES_DIR)
+        instance._make_btb_scorer.assert_called_once_with(
+            'demo-dataset', {'problem': dict()}, mock_lazy_loader.return_value)
+        mock_btb_session.assert_called_once_with(template_loader, btb_scorer, max_errors=5)
+
+        assert session.run.call_args_list == expected_run_call_args_list
+
+    @patch('ta2.core.signal')
+    @patch('ta2.core.get_dataset_details')
+    @patch('ta2.core.get_dataset_name')
+    def test_search_default(self, mock_get_dataset_name, mock_get_dataset_details, mock_signal):
+        # setup
+        instance = MagicMock(autospec=TA2Core)
+        instance.timeout = None
+        instance._select_templates.return_value = ['template_1', 'template_2']
+
+        mock_get_dataset_name.return_value = 'demo-dataset'
+        mock_get_dataset_details.return_value = ('data_modality', 'task_type', 'task_subtype')
+
+        # run
+        result = TA2Core.search(instance, {'dataset': dict()}, {'problem': dict()})
+
+        # assert
+        expected_result = {
+            'summary': instance.summary,
+            'iterations': instance.iterations,
+            'templates': 2,
+            'scored': instance.scored,
+            'errored': instance.errored,
+            'invalid': instance.invalid,
+            'timedout': instance.timedout,
+            'killed': instance.killed
+        }
+
+        mock_get_dataset_name.assert_called_once_with({'problem': dict()})
+        mock_get_dataset_details.assert_called_once_with({'dataset': dict()}, {'problem': dict()})
+
+        instance._setup_search.assert_called_once_with(None)
+
+        instance._select_templates.assert_called_once_with(
+            'demo-dataset', 'data_modality', 'task_type', TEMPLATES_CSV)
+
+        instance._start_session.assert_called_once_with(
+            ['template_1', 'template_2'], {'dataset': dict()}, {'problem': dict()}, None)
+
+        mock_signal.alarm.assert_not_called()
+
+        assert result == expected_result
+        assert instance.done
+
+    @patch('ta2.core.signal')
+    @patch('ta2.core.get_dataset_details')
+    @patch('ta2.core.get_dataset_name')
+    def test_search_with_arguments(
+            self, mock_get_dataset_name, mock_get_dataset_details, mock_signal):
+
+        # setup
+        instance = MagicMock(autospec=TA2Core)
+        instance.timeout = None
+        instance._select_templates.return_value = ['template_1', 'template_2']
+
+        mock_get_dataset_name.return_value = 'demo-dataset'
+        mock_get_dataset_details.return_value = ('data_modality', 'task_type', 'task_subtype')
+
+        # run
+        result = TA2Core.search(
+            instance,
+            {'dataset': dict()},
+            {'problem': dict()},
+            timeout=720,
+            budget=5,
+            templates_csv=['template_1', 'template_2']
+        )
+
+        # assert
+        expected_result = {
+            'summary': instance.summary,
+            'iterations': instance.iterations,
+            'templates': 2,
+            'scored': instance.scored,
+            'errored': instance.errored,
+            'invalid': instance.invalid,
+            'timedout': instance.timedout,
+            'killed': instance.killed
+        }
+
+        mock_get_dataset_name.assert_called_once_with({'problem': dict()})
+        mock_get_dataset_details.assert_called_once_with({'dataset': dict()}, {'problem': dict()})
+
+        instance._setup_search.assert_called_once_with(720)
+
+        instance._select_templates.assert_called_once_with(
+            'demo-dataset', 'data_modality', 'task_type', ['template_1', 'template_2'])
+
+        instance._start_session.assert_called_once_with(
+            ['template_1', 'template_2'], {'dataset': dict()}, {'problem': dict()}, 5)
+
+        mock_signal.alarm.assert_not_called()
+
+        assert result == expected_result
+        assert instance.done
+
+    @patch('ta2.core.signal')
+    @patch('ta2.core.get_dataset_details')
+    @patch('ta2.core.get_dataset_name')
+    def test_search_stop_tuning_exception(
+            self, mock_get_dataset_name, mock_get_dataset_details, mock_signal):
+        # setup
+        instance = MagicMock(autospec=TA2Core)
+        instance.timeout = None
+        instance._select_templates.return_value = ['template_1', 'template_2']
+        instance._start_session.side_effect = [StopTuning(), None]
+        instance._get_all_templates.return_value = ['template_2', 'template_3', 'template_4']
+
+        mock_get_dataset_name.return_value = 'demo-dataset'
+        mock_get_dataset_details.return_value = ('data_modality', 'task_type', 'task_subtype')
+
+        # run
+        result = TA2Core.search(
+            instance,
+            {'dataset': dict()},
+            {'problem': dict()},
+            timeout=720,
+            budget=5,
+            templates_csv=['template_1', 'template_2']
+        )
+
+        # assert
+        expected_result = {
+            'summary': instance.summary,
+            'iterations': instance.iterations,
+            'templates': 2,
+            'scored': instance.scored,
+            'errored': instance.errored,
+            'invalid': instance.invalid,
+            'timedout': instance.timedout,
+            'killed': instance.killed
+        }
+
+        mock_get_dataset_name.assert_called_once_with({'problem': dict()})
+        mock_get_dataset_details.assert_called_once_with({'dataset': dict()}, {'problem': dict()})
+
+        instance._setup_search.assert_called_once_with(720)
+
+        instance._select_templates.assert_called_once_with(
+            'demo-dataset', 'data_modality', 'task_type', ['template_1', 'template_2'])
+
+        mock_signal.alarm.assert_not_called()
+
+        assert instance._start_session.call_args_list == [
+            call(['template_1', 'template_2'], {'dataset': dict()}, {'problem': dict()}, 5),
+            call(['template_3', 'template_4'], {'dataset': dict()}, {'problem': dict()}, 5)
+        ]
+
+        assert result == expected_result
+        assert instance.done
+
+    @pytest.mark.skip(reason="TODO")
+    def test_search_keyboard_interrupt(self):
+        pass  # TODO
+
+    @patch('ta2.core.LOGGER')
+    @patch('ta2.core.signal')
+    @patch('ta2.core.get_dataset_details')
+    @patch('ta2.core.get_dataset_name')
+    def test_search_exception(
+            self, mock_get_dataset_name, mock_get_dataset_details, mock_signal, mock_logger):
+
+        # setup
+        instance = MagicMock(autospec=TA2Core)
+        instance.timeout = None
+        instance._select_templates.return_value = ['template_1', 'template_2']
+
+        instance._start_session.side_effect = [Exception()]
+
+        mock_get_dataset_name.return_value = 'demo-dataset'
+        mock_get_dataset_details.return_value = ('data_modality', 'task_type', 'task_subtype')
+
+        # run
+        result = TA2Core.search(instance, {'dataset': dict()}, {'problem': dict()})
+
+        # assert
+        expected_result = {
+            'summary': instance.summary,
+            'iterations': instance.iterations,
+            'templates': 2,
+            'scored': instance.scored,
+            'errored': instance.errored,
+            'invalid': instance.invalid,
+            'timedout': instance.timedout,
+            'killed': instance.killed
+        }
+
+        mock_get_dataset_name.assert_called_once_with({'problem': dict()})
+        mock_get_dataset_details.assert_called_once_with({'dataset': dict()}, {'problem': dict()})
+
+        instance._setup_search.assert_called_once_with(None)
+
+        instance._select_templates.assert_called_once_with(
+            'demo-dataset', 'data_modality', 'task_type', TEMPLATES_CSV)
+
+        instance._start_session.assert_called_once_with(
+            ['template_1', 'template_2'], {'dataset': dict()}, {'problem': dict()}, None)
+
+        mock_logger.exception.assert_called_once_with(
+            'Error processing dataset %s', {'dataset': dict()}
+        )
+
+        mock_signal.alarm.assert_not_called()
+
+        assert result == expected_result
+        assert instance.done
